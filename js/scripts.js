34d9fd8e4cbc46cf0d8df897a86a8b6bb7b12677
MajaMaja17 <maja.jelenik@vp.pl>
2019-03-28 13:58:37 +0100

    Add all files

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// nav
+document.getElementById('myTopnav').addEventListener('click', respNav);
+
+function respNav() {
+
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
